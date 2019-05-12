3d94a89590d8991ec6aec4b5d214837338608230
Jonas Häggqvist <rasher@rasher.dk>
2019-05-13 01:29:51 +0200

    Slight improvement of top100 command

--- management/commands/top100.py
+++ management/commands/top100.py
@@ -5,17 +5,32 @@ from draftcardposter.models import Player
 from django.urls import reverse
 from redditnfl.nfltools import nflteams
 from pathlib import Path
+import re
+
+def expand_range(s):
+    r = []
+    for part in s.split(",") if s is not None else []:
+        if part.isdigit():
+            r.append(int(part))
+        elif '-' in part:
+            m = re.match(r"(?P<from>-?[0-9]+)-(?P<to>-?[0-9]+)", part)
+            from_, to = int(m['from']), int(m['to'])+1
+            assert from_ < to, "To is not larger than from: %s" % part
+            r += range(from_, to)
+        else:
+            raise Exception("Unparsable part: %s" % part)
+    return r
+
 
 class Command(BaseCommand):
     def add_arguments(self, parser):
-        parser.add_argument('base_url')
         parser.add_argument('output_dir')
         parser.add_argument('--ranks')
+        parser.add_argument('--baseurl')
 
     def handle(self, *args, **options):
-        ranks = None
-        if options['ranks'] is not None:
-            ranks = list(map(int, options['ranks'].split(',')))
+        ranks = expand_range(options['ranks'])
+        base_url = options['baseurl'] if options['baseurl'] is not None else 'http://localhost:8000'
         def rank(p):
             return int(p.data['RANK'].replace('T-', ''))
 
@@ -24,11 +39,16 @@ class Command(BaseCommand):
         output_dir.mkdir(parents=True, exist_ok=True)
         
         for p in players:
-            if ranks is not None and rank(p) not in ranks:
+            if ranks and rank(p) not in ranks:
+                continue
+            if not p.name:
                 continue
             team = nflteams.get_team(next(filter(lambda x: x[1]==p.data['TEAM'], nflteams.fullnames.items()))[0])
-            url = options['base_url'] + reverse('player-card', kwargs={'overall':1, 'team':team['short'], 'pos':p.position, 'name':p.name, 'college': p.college, 'fmt':'png'})
+            url = base_url + reverse('player-card', kwargs={'overall':1, 'team':team['short'], 'pos':p.position, 'name':p.name, 'college': p.college, 'fmt':'png'})
             fn = output_dir / Path("{rank:03d} {p.name}.png".format(p=p, rank=rank(p)))
+            if fn.exists():
+                continue
             with open(fn, 'wb') as out:
+                print(url)
                 out.write(urlopen(url).read())
-                print(fn)
+                print(" -> %s" % fn)
