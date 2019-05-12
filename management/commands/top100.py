9439cf69b703ec88a7584314398ac4f575b3b2ad
Jonas Häggqvist <rasher@rasher.dk>
2019-05-13 00:08:02 +0200

    Fix top100 generating command

--- management/commands/top100.py
+++ management/commands/top100.py
@@ -3,7 +3,8 @@ from urllib.request import urlopen
 import pprint
 from draftcardposter.models import Player
 from django.urls import reverse
-import nflteams
+from redditnfl.nfltools import nflteams
+from pathlib import Path
 
 class Command(BaseCommand):
     def add_arguments(self, parser):
@@ -19,14 +20,15 @@ class Command(BaseCommand):
             return int(p.data['RANK'].replace('T-', ''))
 
         players = sorted(Player.objects.all(), key=rank)
-        output_dir = options['output_dir']
+        output_dir = Path(options['output_dir'])
+        output_dir.mkdir(parents=True, exist_ok=True)
         
         for p in players:
             if ranks is not None and rank(p) not in ranks:
                 continue
-            team = nflteams.findteam(p.data['TEAM'])
+            team = nflteams.get_team(next(filter(lambda x: x[1]==p.data['TEAM'], nflteams.fullnames.items()))[0])
             url = options['base_url'] + reverse('player-card', kwargs={'overall':1, 'team':team['short'], 'pos':p.position, 'name':p.name, 'college': p.college, 'fmt':'png'})
-            fn = "{output_dir}/{rank:03d} {p.name}.jpg".format(p=p, rank=rank(p), output_dir=output_dir)
+            fn = output_dir / Path("{rank:03d} {p.name}.png".format(p=p, rank=rank(p)))
             with open(fn, 'wb') as out:
                 out.write(urlopen(url).read())
                 print(fn)
